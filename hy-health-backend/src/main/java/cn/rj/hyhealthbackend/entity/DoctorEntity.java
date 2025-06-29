b8e0132bec967d23361df9829e3bf9c8786a1729
Coki_Lee <14337294+coki-lee@user.noreply.gitee.com>
2025-06-29 17:02:07 +0800

    feat(domain): 添加医师相关实体类和模型类
    - 新增 Doctor 实体类，包含医师基本信息和验证注解
    - 创建 DoctorEntity 数据访问类
    - 添加 DoctorLevelModel 和 DoctorModel 用于前端数据返回
    - 实现 DoctorParam医师查询参数类，包含密码属性

---
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/entity/DoctorEntity.java
@@ -0,0 +1,9 @@
+package cn.rj.hyhealthbackend.entity;
+
+/**
+ * @author 李柯嘉
+ *
+ * 医师数据访问类
+ */
+public class DoctorEntity {
+}
---
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/model/DoctorLevelModel.java
@@ -0,0 +1,11 @@
+package cn.rj.hyhealthbackend.model;
+
+import cn.rj.hyhealthbackend.domain.DoctorLevel;
+
+/**
+ * @author 李柯嘉
+ *
+ * 用于返回前端数据
+ */
+public class DoctorLevelModel extends DoctorLevel {
+}
