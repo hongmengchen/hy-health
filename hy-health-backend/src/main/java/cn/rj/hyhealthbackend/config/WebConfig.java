8d947c156e9ca12f39ddca8a06e4ffd0b24b1f32
hongmengchen <2560174991@qq.com>
2025-06-29 19:14:55 +0800

    feat(config): 添加 Web 配置类
    - 新增 WebConfig 类，实现跨域配置和响应头时间设置
    - 跨域配置允许所有来源、方法和头部，最长跨域时间为 3600 秒
    - 在响应头中添加当前时间信息

---
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/config/WebConfig.java
@@ -0,0 +1,45 @@
+package cn.rj.hyhealthbackend.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Date;
+
+/**
+ * @author 陈亮
+ * <p>
+ * MVC 配置类
+ * 配置了响应头中的时间为当前时间，并添加了跨域配置，设置了跨域的域名、请求方法以及请求体信息为所有内容，并设置最长跨域时间为 3600毫 秒
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * 设置响应头的时间为GMT+8
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                response.setHeader("date", String.valueOf(new Date()));
+                return true;
+            }
+        }).addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
