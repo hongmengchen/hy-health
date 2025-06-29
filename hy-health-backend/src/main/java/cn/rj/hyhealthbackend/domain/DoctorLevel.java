cf9eca5c1d6ff711f435aee1897d50800de218b1
hongmengchen <2560174991@qq.com>
2025-06-29 17:05:35 +0800

    refactor(domain): 使用 Lombok优化实体类代码
    - 在 Account、DoctorLevel 和 SuperDomain 类中添加 Lombok 注解
    -移除自动生成的 getter、setter 和构造函数
    - 通过 @Data 和 @EqualsAndHashCode 注解实现属性的自动处理
    -简化代码结构，提高可读性和维护性

--- hy-health-backend/src/main/java/cn/rj/hyhealthbackend/domain/DoctorLevel.java
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/domain/DoctorLevel.java
@@ -1,30 +1,17 @@
 package cn.rj.hyhealthbackend.domain;
 
 import cn.rj.hyhealthbackend.domain.superdomain.SuperDomain;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author 梁旭
  *
  * 医师级别实体类
  */
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class DoctorLevel extends SuperDomain {
     private Long id;//级别id
     private String name;//级别名称
-
-    // getter and setter
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
