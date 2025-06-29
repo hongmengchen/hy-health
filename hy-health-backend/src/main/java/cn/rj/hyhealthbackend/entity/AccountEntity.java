92189b7e3827425126e9844131650be4b953c687
hongmengchen <2560174991@qq.com>
2025-06-29 15:14:27 +0800

    feat(entity): 新增 AccountEntity 类
    - 创建 AccountEntity 类，继承自 Account 域对象
    - 用于接收数据库返回信息，作为数据库操作类
    - 作者：陈亮

---
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/entity/AccountEntity.java
@@ -0,0 +1,12 @@
+package cn.rj.hyhealthbackend.entity;
+
+import cn.rj.hyhealthbackend.domain.Account;
+
+/**
+ * @author 陈亮
+ *
+ * 数据库操作类
+ * 用于接收数据库返回信息
+ */
+public class AccountEntity extends Account {
+}
