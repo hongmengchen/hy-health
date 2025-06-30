cc33f9dad51e1d51dc56eb86dbe734db44a35698
hongmengchen <2560174991@qq.com>
2025-06-30 19:43:36 +0800

    添加后端素材

--- hy-health-frontend/main.js
+++ hy-health-frontend/main.js
@@ -2,7 +2,15 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import store from './store' // 引入vuex
 import './uni.promisify.adaptor'
+import _ from 'lodash' // js库
+import './static/reset.css' // 引入样式
+import 'animate.css' // 引入动画库
+
+// 注册全局工具函数
+Vue.prototype._ = _
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
