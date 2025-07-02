c3c698a237a60c772268c8c7cccf6b47104ba1d0
hongmengchen <2560174991@qq.com>
2025-07-02 11:41:19 +0800

    featy(h-health-backend): 添加销售地点的经纬度和地址信息- 在 Sale 实体类中添加 lng、lat 和 address 字段- 更新数据库表结构，增加经纬度和地址列 - 修改 SaleMapper.xml，支持新增字段的查询和插入

--- hy-health-backend/src/main/java/cn/rj/hyhealthbackend/domain/Sale.java
+++ hy-health-backend/src/main/java/cn/rj/hyhealthbackend/domain/Sale.java
@@ -2,13 +2,17 @@ package cn.rj.hyhealthbackend.domain;
 
 import cn.rj.hyhealthbackend.domain.superdomain.SuperDomain;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.Date;
+
 /**
- * @author 李柯嘉
+ * @author 李柯嘉、陈亮
  * <p>
  * 销售地点实体类Sale
+ * - 添加经纬度和地址信息，并添加对应的get/set和构造方法
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class Sale extends SuperDomain {
     private Long saleId;//药店id
@@ -16,4 +20,7 @@ public class Sale extends SuperDomain {
     private String salePhone;//药店电话
     private Date createtime;//创建时间
     private Date updatetime;//修改时间
+    private Double lng;
+    private Double lat;
+    private String address;
 }
